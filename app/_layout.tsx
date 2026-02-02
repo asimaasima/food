e934e66675bb1f2ddb2fac4e80c82827f39d2ed8
asimaasima <asimanihal786@gmail.com>
2026-02-02 17:11:45 -0500

    first day changes

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,5 @@
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
 
 import { useColorScheme } from '@/hooks/use-color-scheme';
@@ -15,10 +14,9 @@ export default function RootLayout() {
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
-      </Stack>
-      <StatusBar style="auto" />
+        <Stack.Screen name="index" options={{ title: "Notes App" }} />
+        <Stack.Screen name="add" options={{ title: "Add note" }} />
+      </Stack>    
     </ThemeProvider>
   );
 }
